a87b6359abff28160ca4a9198d11498dfd69f162
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-07-21 00:18:20 +0200

    CORS and database connection initialization

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,13 +13,19 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+  cors = CORS(app, resources={r'/*': { 'origins': '*' }})
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+  @app.after_request
+  def allow_cors(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS')
+    return response
+
+  @app.route('/')
+  def index():
+    return jsonify({
+      'success': 'true'
+    }), 200
 
   '''
   @TODO: 
