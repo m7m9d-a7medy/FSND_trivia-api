0d8b94d93ebef73b5268bf82ce4f41b5ce26db77
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-07-22 12:38:32 +0200

    Added error handlers for 400, 500

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -148,14 +148,32 @@ def create_app(test_config=None):
     def not_found(error):
         return jsonify({
             'success': False,
+            'error': 404,
             'message': 'Requested resource not found'
         }), 404
 
     @app.errorhandler(422)
-    def not_found(error):
+    def unproccessable(error):
         return jsonify({
             'success': False,
+            'error': 422,
             'message': 'Requested resource is unproccessable'
         }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+      return jsonify({
+        'success':False,
+        'error': 400,
+        'message':'Bad Request'
+        }), 400
+
+    @app.errorhandler(500)
+    def server_error(error):
+      return jsonify({
+        'success':False,
+        'error': 500,
+        'message':'Internal server error'
+        }), 500
 
     return app
